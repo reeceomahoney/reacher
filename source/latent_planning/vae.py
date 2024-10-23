840654157c56b7ba71eb7cdfdc12935c7e6aa852
Reece O'Mahoney <reece.omahoney3@gmail.com>
2024-10-23 10:49:05 +0100

    add vae class

---
+++ source/latent_planning/vae.py
@@ -0,0 +1,17 @@
+import torch
+import torch.nn as nn
+
+
+class VAE(nn.Module):
+    def __init__(self, input_dim, latent_dim, device):
+        super().__init__()
+        self.net = nn.Linear(input_dim, latent_dim)
+        self.input_dim = input_dim
+        self.latent_dim = latent_dim
+        self.device = device
+
+    def act(self, x):
+        return torch.zeros((x.shape[0], 7), device=self.device)
+
+    def update(self):
+        return 0, 0
