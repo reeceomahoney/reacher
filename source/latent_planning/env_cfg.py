e3b281f2f0f9f34a14dd6e9c294f4bd80f5a6a32
Reece O'Mahoney <reece.omahoney3@gmail.com>
2024-10-28 18:01:16 +0000

    remove unused term

--- source/latent_planning/env_cfg.py
+++ source/latent_planning/env_cfg.py
@@ -174,7 +174,6 @@ class ObservationsCfg:
         )
 
         def __post_init__(self):
-            self.enable_corruption = True
             self.concatenate_terms = True
 
     # observation groups
