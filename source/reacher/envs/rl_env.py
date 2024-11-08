37a1675bf346d6a9a812602e2b2327c544dc096a
Reece O'Mahoney <reece.omahoney3@gmail.com>
2024-11-08 16:38:53 +0000

    change rewards

--- source/reacher/envs/rl_env.py
+++ source/reacher/envs/rl_env.py
@@ -5,11 +5,12 @@
 
 import gymnasium as gym
 import math
+import torch
 from dataclasses import MISSING
 
 import omni.isaac.lab.sim as sim_utils
-from omni.isaac.lab.assets import ArticulationCfg, AssetBaseCfg
-from omni.isaac.lab.envs import ManagerBasedRLEnvCfg
+from omni.isaac.lab.assets import ArticulationCfg, AssetBaseCfg, RigidObject
+from omni.isaac.lab.envs import ManagerBasedRLEnv, ManagerBasedRLEnvCfg
 from omni.isaac.lab.managers import CurriculumTermCfg as CurrTerm
 from omni.isaac.lab.managers import EventTermCfg as EventTerm
 from omni.isaac.lab.managers import ObservationGroupCfg as ObsGroup
@@ -22,6 +23,11 @@ from omni.isaac.lab.sensors import ContactSensorCfg, RayCasterCfg, patterns
 from omni.isaac.lab.terrains import TerrainImporterCfg
 from omni.isaac.lab.utils import configclass
 from omni.isaac.lab.utils.assets import ISAAC_NUCLEUS_DIR, ISAACLAB_NUCLEUS_DIR
+from omni.isaac.lab.utils.math import (
+    combine_frame_transforms,
+    quat_error_magnitude,
+    quat_mul,
+)
 from omni.isaac.lab.utils.noise import AdditiveUniformNoiseCfg as Unoise
 from reacher.envs.base_env import ee_pos_rot
 
@@ -38,6 +44,49 @@ from reacher.robots import ANYMAL_D_Z1_CFG  # isort: skip
 ##
 # Scene definition
 ##
+
+
+def position_command_error(
+    env: ManagerBasedRLEnv,
+    command_name: str,
+    asset_cfg: SceneEntityCfg,
+    sigma: float = 0.1,
+) -> torch.Tensor:
+    """Penalize tracking of the position error using L2-norm.
+
+    The function computes the position error between the desired position (from the command) and the
+    current position of the asset's body (in world frame). The position error is computed as the L2-norm
+    of the difference between the desired and current positions.
+    """
+    # extract the asset (to enable type hinting)
+    asset: RigidObject = env.scene[asset_cfg.name]
+    command = env.command_manager.get_command(command_name)
+    # obtain the desired and current positions
+    des_pos_b = command[:, :3]
+    des_pos_w, _ = combine_frame_transforms(
+        asset.data.root_state_w[:, :3], asset.data.root_state_w[:, 3:7], des_pos_b
+    )
+    curr_pos_w = asset.data.body_state_w[:, asset_cfg.body_ids[0], :3]  # type: ignore
+    return torch.exp(-((torch.norm(curr_pos_w - des_pos_w, dim=1) / sigma) ** 2))
+
+
+def orientation_command_error(
+    env: ManagerBasedRLEnv, command_name: str, asset_cfg: SceneEntityCfg
+) -> torch.Tensor:
+    """Penalize tracking orientation error using shortest path.
+
+    The function computes the orientation error between the desired orientation (from the command) and the
+    current orientation of the asset's body (in world frame). The orientation error is computed as the shortest
+    path between the desired and current orientations.
+    """
+    # extract the asset (to enable type hinting)
+    asset: RigidObject = env.scene[asset_cfg.name]
+    command = env.command_manager.get_command(command_name)
+    # obtain the desired and current orientations
+    des_quat_b = command[:, 3:7]
+    des_quat_w = quat_mul(asset.data.root_state_w[:, 3:7], des_quat_b)
+    curr_quat_w = asset.data.body_state_w[:, asset_cfg.body_ids[0], 3:7]  # type: ignore
+    return torch.exp(-quat_error_magnitude(curr_quat_w, des_quat_w))
 
 
 @configclass
@@ -280,16 +329,16 @@ class RewardsCfg:
     #     params={"command_name": "base_velocity", "std": math.sqrt(0.25)},
     # )
     end_effector_position_tracking = RewTerm(
-        func=manip_mdp.position_command_error,
-        weight=-1,
+        func=position_command_error,
+        weight=1,
         params={
             "asset_cfg": SceneEntityCfg("robot", body_names="gripperMover"),
             "command_name": "ee_pose",
         },
     )
     end_effector_orientation_tracking = RewTerm(
-        func=manip_mdp.orientation_command_error,
-        weight=-1,
+        func=orientation_command_error,
+        weight=1,
         params={
             "asset_cfg": SceneEntityCfg("robot", body_names="gripperMover"),
             "command_name": "ee_pose",
