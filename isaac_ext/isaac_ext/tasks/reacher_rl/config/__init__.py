006c38097a9a648f4d3b92c1aa6923d4f3bbc42e
Reece O'Mahoney <reece.omahoney3@gmail.com>
2024-11-18 18:26:09 +0000

    add new env

--- isaac_ext/isaac_ext/tasks/reacher_rl/config/__init__.py
+++ isaac_ext/isaac_ext/tasks/reacher_rl/config/__init__.py
@@ -1,5 +1,6 @@
 import gymnasium as gym
 from . import rl_env_cfg
+from .locodiff import locodiff_env_cfg
 
 gym.register(
     id="Isaac-Reacher-RL",
@@ -37,5 +38,14 @@ gym.register(
     kwargs={
         "env_cfg_entry_point": rl_env_cfg.ReacherRLFlatEnvCfg_PLAY,
         "agent_cfg_entry_point": "reacher.config.rl_cfg:ReacherFlatPPORunnerCfg",
+    },
+)
+gym.register(
+    id="Isaac-Locodiff",
+    entry_point="omni.isaac.lab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": locodiff_env_cfg.LocodiffEnvCfg,
+        "agent_cfg_entry_point": "reacher.config.rl_cfg:ReacherPPORunnerCfg",
     },
 )
