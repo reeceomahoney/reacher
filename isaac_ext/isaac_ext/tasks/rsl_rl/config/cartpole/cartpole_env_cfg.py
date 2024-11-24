7be511ba27f2b87e78d040d1c3d0511a2f7b76c3
Reece O'Mahoney <reece.omahoney3@gmail.com>
2024-11-24 15:41:17 +0000

    change env cfg

--- isaac_ext/isaac_ext/tasks/rsl_rl/config/cartpole/cartpole_env_cfg.py
+++ isaac_ext/isaac_ext/tasks/rsl_rl/config/cartpole/cartpole_env_cfg.py
@@ -10,6 +10,9 @@ from omni.isaac.lab_tasks.manager_based.classic.cartpole.cartpole_env_cfg import
 class DiffusionCartpoleEnvCfg(CartpoleEnvCfg):
     def __post_init__(self):
         super().__post_init__()
-        self.actions.joint_effort.scale = 5.0
+        # robot
+        self.robot.actuators["cart_actuator"].effort_limit = 100.0
+        self.robot.actuators["pole_actuator"].effort_limit = 100.0
+        # mdp
+        self.actions.joint_effort.scale = 100.0
         self.events.reset_pole_position.params["position_range"] = (-math.pi, math.pi)
-        self.terminations.cart_out_of_bounds.params["bounds"] = (-6.0, 6.0)
