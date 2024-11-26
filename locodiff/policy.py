26e5093843118a2a2853278965de1704e96db177
Reece O'Mahoney <reece.omahoney3@gmail.com>
2024-11-26 11:56:46 +0000

    change inpainting

--- locodiff/policy.py
+++ locodiff/policy.py
@@ -167,12 +167,12 @@ class DiffusionPolicy(nn.Module):
             mask[:, : self.T_cond, : self.obs_dim] = 1.0
         if self.inpaint_final_obs:
             if data["input"] is None:
-                tgt_pos = torch.tensor([1.0]).to(self.device)
+                tgt_pos = torch.tensor([1.0, 0.0, 0.0, 0.0]).to(self.device)
                 tgt_pos = self.normalizer.scale_pos(tgt_pos)
-                tgt[:, -1, 0] = tgt_pos
+                tgt[:, -1, : self.obs_dim] = tgt_pos
             else:
-                tgt[:, -1, 0] = data["input"][:, -1, 0]
-            mask[:, -1, 0] = 1.0
+                tgt[:, -1, : self.obs_dim] = data["input"][:, -1, : self.obs_dim]
+            mask[:, -1, : self.obs_dim] = 1.0
 
         return tgt, mask
 
