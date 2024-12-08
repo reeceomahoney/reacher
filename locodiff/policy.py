fix goal scaling and add seed

locodiff/policy.py
@@ -6,11 +6,11 @@ import torch.nn as nn
 from torch.optim.adamw import AdamW
 from torch.optim.lr_scheduler import CosineAnnealingLR
 
+import wandb
 from diffusers.schedulers.scheduling_edm_dpmsolver_multistep import (
     EDMDPMSolverMultistepScheduler,
 )
 
-import wandb
 from locodiff.utils import CFGWrapper, apply_conditioning, rand_log_logistic
 
 
@@ -209,7 +209,7 @@ class DiffusionPolicy(nn.Module):
             data = self.update_history(data)
             raw_obs = data["obs"]
             input = None
-            goal = self.normalizer.scale_pos(self.goal)
+            goal = self.normalizer.scale_input(self.goal)
         else:
             # train and test
             raw_obs = data["obs"]

locodiff/utils.py
@@ -226,8 +226,8 @@ class Normalizer(nn.Module):
     def scale_pos(self, x) -> torch.Tensor:
         dim = x.shape[-1]
         if self.scaling == "linear":
-            return (x - self.y_min[:dim]) / (
-                self.y_max[:dim] - self.y_min[:dim]
+            return (x - self.y_min[2:]) / (
+                self.y_max[2:] - self.y_min[2:]
             ) * 2 - 1
         elif self.scaling == "gaussian":
             return (x - self.y_mean[:dim]) / self.y_std[:dim]

scripts/maze/play.py
@@ -1,10 +1,11 @@
 import matplotlib.pyplot as plt
-import statistics
-from tqdm import tqdm
 import numpy as np
 import os
+import random
+import statistics
 import sys
 import torch
+from tqdm import tqdm
 
 import hydra
 from hydra.core.hydra_config import HydraConfig
@@ -32,6 +33,11 @@ def main(agent_cfg: DictConfig):
     # specify directory for logging experiments
     log_dir = HydraConfig.get().runtime.output_dir
     print(f"[INFO] Logging experiment in directory: {log_dir}")
+
+    # set random seed
+    random.seed(agent_cfg.seed)
+    np.random.seed(agent_cfg.seed)
+    torch.manual_seed(agent_cfg.seed)
 
     # create isaac environment
     env = MazeEnv(agent_cfg, render=True)