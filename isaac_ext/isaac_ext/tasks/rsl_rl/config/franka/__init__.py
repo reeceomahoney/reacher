312d117c92d7e817d1a08cccc093f63f4d0f4dd2
Reece O'Mahoney <reece.omahoney3@gmail.com>
2025-01-24 10:43:57 +0000

    change reset config

--- isaac_ext/isaac_ext/tasks/rsl_rl/config/franka/__init__.py
+++ isaac_ext/isaac_ext/tasks/rsl_rl/config/franka/__init__.py
@@ -2,15 +2,14 @@ import os
 
 import gymnasium as gym
 
-from . import env_cfg, env_cfg_2
+from . import env_cfg
 
 gym.register(
     id="Isaac-Franka-RL",
     entry_point="omni.isaac.lab.envs:ManagerBasedRLEnv",
     disable_env_checker=True,
     kwargs={
-        # "env_cfg_entry_point": env_cfg.FrankaRLEnvCfg,
-        "env_cfg_entry_point": env_cfg_2.FrankaReachEnvCfg,
+        "env_cfg_entry_point": env_cfg.FrankaReachEnvCfg,
         "agent_cfg_entry_point": f"{os.path.dirname(__file__)}/cfg.yaml",
     },
 )
--- isaac_ext/isaac_ext/tasks/rsl_rl/config/franka/env_cfg.py
+++ isaac_ext/isaac_ext/tasks/rsl_rl/config/franka/env_cfg.py
@@ -5,169 +5,18 @@
 
 import math
 
-import omni.isaac.lab.sim as sim_utils
-from omni.isaac.lab.assets import AssetBaseCfg
-from omni.isaac.lab.envs import ManagerBasedRLEnvCfg
-from omni.isaac.lab.managers import ActionTermCfg as ActionTerm
 from omni.isaac.lab.managers import EventTermCfg as EventTerm
-from omni.isaac.lab.managers import ObservationGroupCfg as ObsGroup
-from omni.isaac.lab.managers import ObservationTermCfg as ObsTerm
-from omni.isaac.lab.managers import RewardTermCfg as RewTerm
-from omni.isaac.lab.managers import SceneEntityCfg
-from omni.isaac.lab.managers import TerminationTermCfg as DoneTerm
-from omni.isaac.lab.scene import InteractiveSceneCfg
 from omni.isaac.lab.utils import configclass
-from omni.isaac.lab.utils.assets import ISAAC_NUCLEUS_DIR
-from omni.isaac.lab_assets import FRANKA_PANDA_CFG
+from omni.isaac.lab_tasks.manager_based.manipulation.reach.reach_env_cfg import (
+    ReachEnvCfg,
+)
 
 import isaac_ext.tasks.rsl_rl.mdp as mdp
 
 ##
-# Scene definition
+# Pre-defined configs
 ##
-
-
-@configclass
-class FrankaRLSceneCfg(InteractiveSceneCfg):
-    """Configuration for the scene with a robotic arm."""
-
-    # world
-    ground = AssetBaseCfg(
-        prim_path="/World/ground",
-        spawn=sim_utils.GroundPlaneCfg(),
-        init_state=AssetBaseCfg.InitialStateCfg(pos=(0.0, 0.0, -1.05)),
-    )
-
-    table = AssetBaseCfg(
-        prim_path="{ENV_REGEX_NS}/Table",
-        spawn=sim_utils.UsdFileCfg(
-            usd_path=f"{ISAAC_NUCLEUS_DIR}/Props/Mounts/SeattleLabTable/table_instanceable.usd",
-        ),
-        init_state=AssetBaseCfg.InitialStateCfg(
-            pos=(0.55, 0.0, 0.0), rot=(0.70711, 0.0, 0.0, 0.70711)
-        ),
-    )
-
-    # robots
-    robot = FRANKA_PANDA_CFG.replace(prim_path="{ENV_REGEX_NS}/Robot")  # type: ignore
-
-    # lights
-    light = AssetBaseCfg(
-        prim_path="/World/light",
-        spawn=sim_utils.DomeLightCfg(color=(0.75, 0.75, 0.75), intensity=2500.0),
-    )
-
-
-##
-# MDP settings
-##
-
-
-@configclass
-class CommandsCfg:
-    """Command terms for the MDP."""
-
-    ee_pose = mdp.UniformPoseCommandCfg(
-        asset_name="robot",
-        body_name="panda_hand",
-        resampling_time_range=(12.0, 12.0),
-        debug_vis=True,
-        ranges=mdp.UniformPoseCommandCfg.Ranges(
-            pos_x=(0.35, 0.65),
-            pos_y=(-0.2, 0.2),
-            pos_z=(0.15, 0.5),
-            roll=(0.0, 0.0),
-            pitch=(math.pi, math.pi),
-            yaw=(-3.14, 3.14),
-        ),
-    )
-
-
-@configclass
-class ActionsCfg:
-    """Action specifications for the MDP."""
-
-    arm_action = mdp.JointPositionActionCfg(
-        asset_name="robot",
-        joint_names=["panda_joint.*"],
-        scale=1.0,
-    )
-    gripper_action: ActionTerm | None = None
-
-
-@configclass
-class ObservationsCfg:
-    """Observation specifications for the MDP."""
-
-    @configclass
-    class PolicyCfg(ObsGroup):
-        """Observations for policy group."""
-
-        joint_pos = ObsTerm(
-            func=mdp.joint_pos,
-            params={"asset_cfg": SceneEntityCfg("robot", joint_names="panda_joint.*")},
-        )
-        joint_vel = ObsTerm(func=mdp.joint_vel_rel)
-        ee_state = ObsTerm(
-            func=mdp.ee_pose,
-            params={"asset_cfg": SceneEntityCfg("robot", body_names="panda_hand")},
-        )
-
-        def __post_init__(self):
-            self.concatenate_terms = True
-
-    # observation groups
-    policy: PolicyCfg = PolicyCfg()
-
-
-@configclass
-class EventCfg:
-    """Configuration for events."""
-
-    reset_robot_joints = EventTerm(
-        func=mdp.reset_joints_random,
-        mode="reset",
-        params={"asset_cfg": SceneEntityCfg("robot", joint_names="panda_joint.*")},
-    )
-
-
-@configclass
-class RewardsCfg:
-    """Reward terms for the MDP."""
-
-    # task
-    ee_tracking = RewTerm(
-        func=mdp.ee_9d_pose_error,
-        weight=0.2,
-        params={
-            "asset_cfg": SceneEntityCfg("robot", body_names="panda_hand"),
-            "command_name": "ee_pose",
-            "std": 1,
-        },
-    )
-    ee_tracking_fine_grained = RewTerm(
-        func=mdp.ee_9d_pose_error,
-        weight=0.1,
-        params={
-            "asset_cfg": SceneEntityCfg("robot", body_names="panda_hand"),
-            "command_name": "ee_pose",
-            "std": 0.1,
-        },
-    )
-    # penalties
-    action_rate = RewTerm(func=mdp.action_rate_l2, weight=-0.01)
-    joint_vel = RewTerm(
-        func=mdp.joint_vel_l2,
-        weight=-0.01,
-        params={"asset_cfg": SceneEntityCfg("robot")},
-    )
-
-
-@configclass
-class TerminationsCfg:
-    """Termination terms for the MDP."""
-
-    time_out = DoneTerm(func=mdp.time_out, time_out=True)
+from omni.isaac.lab_assets import FRANKA_PANDA_CFG  # isort: skip
 
 
 ##
@@ -176,25 +25,53 @@ class TerminationsCfg:
 
 
 @configclass
-class FrankaRLEnvCfg(ManagerBasedRLEnvCfg):
-    """Configuration for the reach end-effector pose tracking environment."""
+class EventCfg:
+    """Configuration for events."""
 
-    # Scene settings
-    scene: FrankaRLSceneCfg = FrankaRLSceneCfg(num_envs=4096, env_spacing=2.5)
-    # Basic settings
-    observations: ObservationsCfg = ObservationsCfg()
-    actions: ActionsCfg = ActionsCfg()
-    commands: CommandsCfg = CommandsCfg()
-    # MDP settings
-    rewards: RewardsCfg = RewardsCfg()
-    terminations: TerminationsCfg = TerminationsCfg()
+    reset_robot_joints = EventTerm(func=mdp.reset_joints_random, mode="reset")
+
+
+@configclass
+class FrankaReachEnvCfg(ReachEnvCfg):
     events: EventCfg = EventCfg()
 
     def __post_init__(self):
-        """Post initialization."""
-        # general settings
-        self.decimation = 5
-        self.sim.render_interval = self.decimation
-        self.sim.dt = 1.0 / 50.0
-        self.episode_length_s = 10.0
-        self.viewer.eye = (3.5, 3.5, 3.5)
+        # post init of parent
+        super().__post_init__()
+
+        # switch robot to franka
+        self.scene.robot = FRANKA_PANDA_CFG.replace(prim_path="{ENV_REGEX_NS}/Robot")
+        # override rewards
+        self.rewards.end_effector_position_tracking.params["asset_cfg"].body_names = [
+            "panda_hand"
+        ]
+        self.rewards.end_effector_position_tracking_fine_grained.params[
+            "asset_cfg"
+        ].body_names = ["panda_hand"]
+        self.rewards.end_effector_orientation_tracking.params[
+            "asset_cfg"
+        ].body_names = ["panda_hand"]
+
+        # override actions
+        self.actions.arm_action = mdp.JointPositionActionCfg(
+            asset_name="robot",
+            joint_names=["panda_joint.*"],
+            scale=0.5,
+            use_default_offset=True,
+        )
+        # override command generator body
+        # end-effector is along z-direction
+        self.commands.ee_pose.body_name = "panda_hand"
+        self.commands.ee_pose.ranges.pitch = (math.pi, math.pi)
+
+
+@configclass
+class FrankaReachEnvCfg_PLAY(FrankaReachEnvCfg):
+    def __post_init__(self):
+        # post init of parent
+        super().__post_init__()
+        # make a smaller scene for play
+        self.scene.num_envs = 50
+        self.scene.env_spacing = 2.5
+        # disable randomization for play
+        self.observations.policy.enable_corruption = False
