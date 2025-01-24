41c9f52434eff9932e67e80677b1a0552e0a9719
Reece O'Mahoney <reece.omahoney3@gmail.com>
2025-01-24 10:20:24 +0000

    add new env cfg

--- isaac_ext/isaac_ext/tasks/rsl_rl/config/franka/__init__.py
+++ isaac_ext/isaac_ext/tasks/rsl_rl/config/franka/__init__.py
@@ -2,14 +2,15 @@ import os
 
 import gymnasium as gym
 
-from . import env_cfg
+from . import env_cfg, env_cfg_2
 
 gym.register(
     id="Isaac-Franka-RL",
     entry_point="omni.isaac.lab.envs:ManagerBasedRLEnv",
     disable_env_checker=True,
     kwargs={
-        "env_cfg_entry_point": env_cfg.FrankaRLEnvCfg,
+        # "env_cfg_entry_point": env_cfg.FrankaRLEnvCfg,
+        "env_cfg_entry_point": env_cfg_2.FrankaReachEnvCfg,
         "agent_cfg_entry_point": f"{os.path.dirname(__file__)}/cfg.yaml",
     },
 )
