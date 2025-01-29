fix ee pose scaling

locodiff/locodiff/policy.py
@@ -316,7 +316,7 @@ class DiffusionPolicy(nn.Module):
             input, returns = None, None
             raw_obs = data["obs"].unsqueeze(1)
             obstacle = self.normalizer.scale_3d_pos(data["obstacle"])
-            goal = self.normalizer.scale_3d_pos(data["goal"])
+            goal = self.normalizer.scale_9d_pos(data["goal"])
         else:
             # train and test
             raw_obs = data["obs"]