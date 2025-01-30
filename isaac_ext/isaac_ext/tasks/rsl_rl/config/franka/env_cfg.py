0d4a01ebe18d241db847e838f8641bdef4a19585
Reece O'Mahoney <reece.omahoney3@gmail.com>
2025-01-30 18:15:01 +0000

    change cfg

--- isaac_ext/isaac_ext/tasks/rsl_rl/config/franka/env_cfg.py
+++ isaac_ext/isaac_ext/tasks/rsl_rl/config/franka/env_cfg.py
@@ -6,7 +6,7 @@ from omni.isaac.lab.managers import ObservationTermCfg as ObsTerm
 from omni.isaac.lab.managers import SceneEntityCfg
 from omni.isaac.lab.utils import configclass
 from omni.isaac.lab.utils.noise import AdditiveUniformNoiseCfg as Unoise
-from omni.isaac.lab_assets import FRANKA_PANDA_HIGH_PD_CFG
+from omni.isaac.lab_assets import FRANKA_PANDA_CFG
 from omni.isaac.lab_tasks.manager_based.manipulation.reach.reach_env_cfg import (
     ReachEnvCfg,
 )
@@ -103,7 +103,7 @@ class FrankaReachEnvCfg(ReachEnvCfg):
         self.sim.dt = 1.0 / 50.0
 
         # switch robot to franka
-        self.scene.robot = FRANKA_PANDA_HIGH_PD_CFG.replace(
+        self.scene.robot = FRANKA_PANDA_CFG.replace(
             prim_path="{ENV_REGEX_NS}/Robot"
         )
         # override rewards
@@ -117,8 +117,8 @@ class FrankaReachEnvCfg(ReachEnvCfg):
             "asset_cfg"
         ].body_names = ["panda_hand"]
         self.rewards.end_effector_orientation_tracking.weight = -0.05
-        self.rewards.action_rate.weight = -0.1
-        self.rewards.joint_vel.weight = -0.1
+        self.rewards.action_rate.weight = -1
+        self.rewards.joint_vel.weight = -1
 
         # override actions
         self.actions.arm_action = mdp.JointPositionActionCfg(
