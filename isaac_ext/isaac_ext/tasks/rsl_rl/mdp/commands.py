2c242573c6964e3d3f60534820eb3ee6dcda20d7
Reece O'Mahoney <reece.omahoney3@gmail.com>
2025-02-11 18:43:22 +0000

    make new command mananger (WIP)

--- isaac_ext/isaac_ext/tasks/rsl_rl/mdp/commands.py
+++ isaac_ext/isaac_ext/tasks/rsl_rl/mdp/commands.py
@@ -8,28 +8,35 @@
 from __future__ import annotations
 
 from collections.abc import Sequence
-from dataclasses import MISSING
-from typing import TYPE_CHECKING
 
 import torch
-import torch.nn.functional as F
+from omegaconf import MISSING
 from omni.isaac.lab.assets import Articulation
-from omni.isaac.lab.managers import CommandTerm, CommandTermCfg
-from omni.isaac.lab.markers import VisualizationMarkers, VisualizationMarkersCfg
+from omni.isaac.lab.envs import ManagerBasedEnv
+from omni.isaac.lab.managers import CommandTerm
+from omni.isaac.lab.managers.manager_term_cfg import CommandTermCfg
+from omni.isaac.lab.markers import VisualizationMarkers
 from omni.isaac.lab.markers.config import FRAME_MARKER_CFG
-from omni.isaac.lab.utils import configclass
-from omni.isaac.lab.utils.math import matrix_from_euler, quat_from_matrix
+from omni.isaac.lab.markers.visualization_markers import VisualizationMarkersCfg
+from omni.isaac.lab.utils.configclass import configclass
+from omni.isaac.lab.utils.math import (
+    combine_frame_transforms,
+    compute_pose_error,
+    quat_from_euler_xyz,
+    quat_unique,
+)
 
-if TYPE_CHECKING:
-    from omni.isaac.lab.envs import ManagerBasedEnv
 
-
-class UniformWorldPoseCommand(CommandTerm):
+class ScheduledPoseCommand(CommandTerm):
     """Command generator for generating pose commands uniformly.
 
     The command generator generates poses by sampling positions uniformly within specified
     regions in cartesian space. For orientation, it samples uniformly the euler angles
-    (roll-pitch-yaw) and converts them into ortho 6d representation.
+    (roll-pitch-yaw) and converts them into quaternion representation (w, x, y, z).
+
+    The position and orientation commands are generated in the base frame of the robot, and not the
+    simulation world frame. This means that users need to handle the transformation from the
+    base frame to the simulation world frame themselves.
 
     .. caution::
 
@@ -39,10 +46,10 @@ class UniformWorldPoseCommand(CommandTerm):
 
     """
 
-    cfg: UniformWorldPoseCommandCfg
+    cfg: ScheduledPoseCommandCfg
     """Configuration for the command generator."""
 
-    def __init__(self, cfg: UniformWorldPoseCommandCfg, env: ManagerBasedEnv):
+    def __init__(self, cfg: ScheduledPoseCommand, env: ManagerBasedEnv):
         """Initialize the command generator class.
 
         Args:
@@ -55,12 +62,12 @@ class UniformWorldPoseCommand(CommandTerm):
         # extract the robot and body index for which the command is generated
         self.robot: Articulation = env.scene[cfg.asset_name]
         self.body_idx = self.robot.find_bodies(cfg.body_name)[0][0]
-        self.env_origins = env.scene.env_origins
 
         # create buffers
-        # -- commands: (x, y, z, ortho 6d) in env frame
-        self.pose_command_l = torch.zeros(self.num_envs, 9, device=self.device)
-        self.pose_command_w = torch.zeros_like(self.pose_command_l)
+        # -- commands: (x, y, z, qw, qx, qy, qz) in root frame
+        self.pose_command_b = torch.zeros(self.num_envs, 7, device=self.device)
+        self.pose_command_b[:, 3] = 1.0
+        self.pose_command_w = torch.zeros_like(self.pose_command_b)
         # -- metrics
         self.metrics["position_error"] = torch.zeros(self.num_envs, device=self.device)
         self.metrics["orientation_error"] = torch.zeros(
@@ -79,11 +86,11 @@ class UniformWorldPoseCommand(CommandTerm):
 
     @property
     def command(self) -> torch.Tensor:
-        """The desired pose command. Shape is (num_envs, 9).
+        """The desired pose command. Shape is (num_envs, 7).
 
-        The first three elements correspond to the position, followed by the ortho 6d orientation.
+        The first three elements correspond to the position, followed by the quaternion orientation in (w, x, y, z).
         """
-        return self.pose_command_l
+        return self.pose_command_b
 
     """
     Implementation specific functions.
@@ -91,35 +98,43 @@ class UniformWorldPoseCommand(CommandTerm):
 
     def _update_metrics(self):
         # transform command from base frame to simulation world frame
-        self.pose_command_w[:, :3] = self.pose_command_l[:, :3] + self.env_origins
-        self.pose_command_w[:, 3:] = self.pose_command_l[:, 3:]
-
+        self.pose_command_w[:, :3], self.pose_command_w[:, 3:] = (
+            combine_frame_transforms(
+                self.robot.data.root_pos_w,
+                self.robot.data.root_quat_w,
+                self.pose_command_b[:, :3],
+                self.pose_command_b[:, 3:],
+            )
+        )
         # compute the error
-        # pos_error, rot_error = compute_pose_error(
-        #     self.pose_command_w[:, :3],
-        #     self.pose_command_w[:, 3:],
-        #     self.robot.data.body_state_w[:, self.body_idx, :3],
-        #     self.robot.data.body_state_w[:, self.body_idx, 3:7],
-        # )
-        # self.metrics["position_error"] = torch.norm(pos_error, dim=-1)
-        # self.metrics["orientation_error"] = torch.norm(rot_error, dim=-1)
+        pos_error, rot_error = compute_pose_error(
+            self.pose_command_w[:, :3],
+            self.pose_command_w[:, 3:],
+            self.robot.data.body_state_w[:, self.body_idx, :3],
+            self.robot.data.body_state_w[:, self.body_idx, 3:7],
+        )
+        self.metrics["position_error"] = torch.norm(pos_error, dim=-1)
+        self.metrics["orientation_error"] = torch.norm(rot_error, dim=-1)
 
     def _resample_command(self, env_ids: Sequence[int]):
         # sample new pose targets
         # -- position
         r = torch.empty(len(env_ids), device=self.device)
-        self.pose_command_l[env_ids, 0] = r.uniform_(*self.cfg.ranges.pos_x)
-        self.pose_command_l[env_ids, 1] = r.uniform_(*self.cfg.ranges.pos_y)
-        self.pose_command_l[env_ids, 2] = r.uniform_(*self.cfg.ranges.pos_z)
+        self.pose_command_b[env_ids, 0] = r.uniform_(*self.cfg.ranges.pos_x)
+        self.pose_command_b[env_ids, 1] = r.uniform_(*self.cfg.ranges.pos_y)
+        self.pose_command_b[env_ids, 2] = r.uniform_(*self.cfg.ranges.pos_z)
         # -- orientation
-        euler_angles = torch.zeros_like(self.pose_command_l[env_ids, :3])
+        euler_angles = torch.zeros_like(self.pose_command_b[env_ids, :3])
         euler_angles[:, 0].uniform_(*self.cfg.ranges.roll)
         euler_angles[:, 1].uniform_(*self.cfg.ranges.pitch)
         euler_angles[:, 2].uniform_(*self.cfg.ranges.yaw)
-        rot_mat = matrix_from_euler(euler_angles, "XYZ")
-        ortho6d = rot_mat[..., :2].reshape(-1, 6)
+        quat = quat_from_euler_xyz(
+            euler_angles[:, 0], euler_angles[:, 1], euler_angles[:, 2]
+        )
         # make sure the quaternion has real part as positive
-        self.pose_command_l[env_ids, 3:] = ortho6d
+        self.pose_command_b[env_ids, 3:] = (
+            quat_unique(quat) if self.cfg.make_quat_unique else quat
+        )
 
     def _update_command(self):
         pass
@@ -151,42 +166,19 @@ class UniformWorldPoseCommand(CommandTerm):
             return
         # update the markers
         # -- goal pose
-        rot_mat = self._rotation_6d_to_matrix(self.pose_command_w[:, 3:])
-        quat = quat_from_matrix(rot_mat)
-        self.goal_pose_visualizer.visualize(self.pose_command_w[:, :3], quat)
+        self.goal_pose_visualizer.visualize(
+            self.pose_command_w[:, :3], self.pose_command_w[:, 3:]
+        )
         # -- current body pose
         body_pose_w = self.robot.data.body_state_w[:, self.body_idx]
         self.current_pose_visualizer.visualize(body_pose_w[:, :3], body_pose_w[:, 3:7])
 
-    def _rotation_6d_to_matrix(self, d6: torch.Tensor) -> torch.Tensor:
-        """
-        Converts 6D rotation representation by Zhou et al. [1] to rotation matrix
-        using Gram--Schmidt orthogonalization per Section B of [1].
-        Args:
-            d6: 6D rotation representation, of size (*, 6)
-
-        Returns:
-            batch of rotation matrices of size (*, 3, 3)
-
-        [1] Zhou, Y., Barnes, C., Lu, J., Yang, J., & Li, H.
-        On the Continuity of Rotation Representations in Neural Networks.
-        IEEE Conference on Computer Vision and Pattern Recognition, 2019.
-        Retrieved from http://arxiv.org/abs/1812.07035
-        """
-
-        a1, a2 = d6[..., :3], d6[..., 3:]
-        b1 = F.normalize(a1, dim=-1)
-        b2 = a2 - (b1 * a2).sum(-1, keepdim=True) * b1
-        b2 = F.normalize(b2, dim=-1)
-        b3 = torch.cross(b1, b2, dim=-1)
-        return torch.stack((b1, b2, b3), dim=-2)
-
 
 @configclass
-class UniformWorldPoseCommandCfg(CommandTermCfg):
+class ScheduledPoseCommandCfg(CommandTermCfg):
     """Configuration for uniform pose command generator."""
 
-    class_type: type = UniformWorldPoseCommand
+    class_type: type = ScheduledPoseCommand
 
     asset_name: str = MISSING
     """Name of the asset in the environment for which the commands are generated."""
