ba03385b98e08102ef8cac38ca6b32a235481023
Reece O'Mahoney <reece.omahoney3@gmail.com>
2025-02-22 14:48:10 +0000

    fix dims

--- locodiff/locodiff/models/transformer.py
+++ locodiff/locodiff/models/transformer.py
@@ -27,7 +27,7 @@ class DiffusionTransformer(nn.Module):
         super().__init__()
         # variables
         input_dim = obs_dim + act_dim
-        input_len = T + 3
+        input_len = T + 4
         self.cond_mask_prob = cond_mask_prob
         self.weight_decay = weight_decay
         self.device = device
