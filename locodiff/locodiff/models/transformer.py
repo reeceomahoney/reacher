d90fd402921f821577ae711f6b51a1a1c00253f4
Reece O'Mahoney <reece.omahoney3@gmail.com>
2025-02-25 11:07:07 +0000

    update model

--- locodiff/locodiff/models/transformer.py
+++ locodiff/locodiff/models/transformer.py
@@ -27,7 +27,7 @@ class DiffusionTransformer(nn.Module):
         super().__init__()
         # variables
         input_dim = obs_dim + act_dim
-        input_len = T + 3 if value else T + 3
+        input_len = T + 3 if value else T + 2
         self.cond_mask_prob = cond_mask_prob
         self.weight_decay = weight_decay
         self.device = device
@@ -37,10 +37,11 @@ class DiffusionTransformer(nn.Module):
         # embeddings
         self.x_emb = nn.Linear(input_dim, d_model)
         self.obs_emb = nn.Linear(obs_dim, d_model)
-        self.goal_emb = nn.Sequential(
-            nn.Linear(9, d_model),
-            Rearrange("b d -> b 1 d"),
-        )
+        if value:
+            self.goal_emb = nn.Sequential(
+                nn.Linear(9, d_model),
+                Rearrange("b d -> b 1 d"),
+            )
         self.t_emb = nn.Sequential(
             Rearrange("b 1 1 -> b 1"),
             SinusoidalPosEmb(d_model, device),
@@ -181,10 +182,14 @@ class DiffusionTransformer(nn.Module):
         x_emb = self.x_emb(x)
         t_emb = self.t_emb(t)
         obs_emb = self.obs_emb(data["obs"])
-        goal_emb = self.goal_emb(data["goal"])
+        if self.value:
+            goal_emb = self.goal_emb(data["goal"])
 
         # construct input
-        x = torch.cat([t_emb, obs_emb, goal_emb, x_emb], dim=1)
+        if self.value:
+            x = torch.cat([t_emb, obs_emb, goal_emb, x_emb], dim=1)
+        else:
+            x = torch.cat([t_emb, obs_emb, x_emb], dim=1)
         x += self.pos_emb
 
         # output
