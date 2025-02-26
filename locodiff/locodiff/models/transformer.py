59fa89ca3782e6b3204b771e19890829018451e5
Reece O'Mahoney <reece.omahoney3@gmail.com>
2025-02-26 17:09:32 +0000

    change length

--- locodiff/locodiff/models/transformer.py
+++ locodiff/locodiff/models/transformer.py
@@ -29,7 +29,7 @@ class DiffusionTransformer(nn.Module):
         # input_dim = obs_dim + act_dim
         input_dim = act_dim
         # input_len = T + 3 if value else T + 2
-        input_len = T + 3
+        input_len = T + 2
         self.cond_mask_prob = cond_mask_prob
         self.weight_decay = weight_decay
         self.device = device
