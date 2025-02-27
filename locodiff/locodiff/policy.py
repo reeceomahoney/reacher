8f8eef8ad1322542ba7eb8c834ffa36fd25d4a23
Reece O'Mahoney <reece.omahoney3@gmail.com>
2025-02-27 11:55:50 +0000

    change time dist

--- locodiff/locodiff/policy.py
+++ locodiff/locodiff/policy.py
@@ -97,9 +97,8 @@ class DiffusionPolicy(nn.Module):
         # sample noise and timestep
         x_1 = data["input"]
         x_0 = torch.randn_like(x_1)
-        # samples = self.beta_dist.sample((len(x_1), 1, 1)).to(self.device)
-        # t = 0.999 * (1 - samples)
-        t = torch.rand(x_1.shape[0], 1, 1).to(self.device)
+        samples = self.beta_dist.sample((len(x_1), 1, 1)).to(self.device)
+        t = 0.999 * (1 - samples)
 
         # compute target
         x_t = (1 - t) * x_0 + t * x_1
